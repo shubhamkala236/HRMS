485d17f18de87c687aacf8566ffc76a57d3ae4ae
harshitsan <harshitbhalla.hb@gmail.com>
2022-07-20 23:34:10 +0530

    test

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,11 @@
 const dotEnv  = require("dotenv");
 // console.log(process.env.NODE_ENV, process.env)
-if (process.env.NODE_ENV !== 'employee') {
-    const configFile =  `./.env.${process.env.NODE_ENV}`;
-    dotEnv.config({ path:  configFile });
-} else {
+// if (process.env.NODE_ENV !== 'employee') {
+//     const configFile =  `./.env.${process.env.NODE_ENV}`;
+//     dotEnv.config({ path:  configFile });
+// } else {
     dotEnv.config();
-}
+// }
 module.exports = {
     PORT: process.env.PORT,
     DB_URL: process.env.MONGODB_URI,
