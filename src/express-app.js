14138345ee7c585fb9942d429a3e2efa09075561
shubham <lawkktd@gmail.com>
2022-07-23 02:51:17 +0530

    cookies

--- src/express-app.js
+++ src/express-app.js
@@ -11,7 +11,10 @@ const HandleErrors = require('./utils/error-handler')
 
 
 module.exports = async (app) => {
-    app.use(cors());
+    app.use(cors({
+        origin: '*' ,
+        credentials: true
+      }));
     app.use(express.json());
     app.use(cookieParser())
     
